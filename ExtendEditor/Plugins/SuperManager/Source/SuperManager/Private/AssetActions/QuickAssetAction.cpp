0f4f0755feb10596581d70fdab0bf9b4f0aa84f3
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2023-12-26 09:12:52 +0900

    Add AssetActionUtility

---
+++ ExtendEditor/Plugins/SuperManager/Source/SuperManager/Private/AssetActions/QuickAssetAction.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AssetActions/QuickAssetAction.h"
+
+void UQuickAssetAction::TestFunc()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 8.0f, FColor::Cyan, TEXT("Test"));
+	} 
+}
+
