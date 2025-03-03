21cf9fd6fdff7c9967033ac1da3e1499a1eb99f8
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2025-03-03 18:18:10 +0900

    Add CustomLyra

---
+++ CustomLyra/Source/CustomLyra/CustomLyraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CustomLyraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUSTOMLYRA_API ACustomLyraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
