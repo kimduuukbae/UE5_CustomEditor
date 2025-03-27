d4f476561d2f9a517c972812c2e5e9568e897d65
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2025-03-27 22:23:38 +0900

    Add ForceUpdate

--- CustomLyra/Source/CustomLyra/Character/CLPawnExtensionComponent.cpp
+++ CustomLyra/Source/CustomLyra/Character/CLPawnExtensionComponent.cpp
@@ -1,5 +1,8 @@
 #include "CLPawnExtensionComponent.h"
+#include "CustomLyra/CLGameplayTags.h"
 #include "Components/GameFrameworkComponentManager.h"
+
+const FName UCLPawnExtensionComponent::NAME_ActorFeatureName("PawnExtension");
 
 UCLPawnExtensionComponent::UCLPawnExtensionComponent(const FObjectInitializer& InObjectInitializer) : Super(InObjectInitializer)
 {
@@ -19,4 +22,47 @@ void UCLPawnExtensionComponent::OnRegister()
 	RegisterInitStateFeature();
 
 	TObjectPtr<UGameFrameworkComponentManager> manager = UGameFrameworkComponentManager::GetForActor(GetOwningActor());
+}
+
+void UCLPawnExtensionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BindOnActorInitStateChanged(NAME_None, FGameplayTag(), false);
+
+	TryToChangeInitState(FCLGameplayTags::Get().InitState_Spawned);
+
+	CheckDefaultInitialization();
+}
+
+void UCLPawnExtensionComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	UnregisterInitStateFeature();
+	Super::EndPlay(EndPlayReason);
+}
+
+void UCLPawnExtensionComponent::OnActorInitStateChanged(const FActorInitStateChangedParams& Params)
+{
+}
+
+bool UCLPawnExtensionComponent::CanChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState) const
+{
+	return false;
+}
+
+void UCLPawnExtensionComponent::CheckDefaultInitialization()
+{
+	CheckDefaultInitializationForImplementers();
+
+	const FCLGameplayTags& initTags = FCLGameplayTags::Get();
+
+	static const TArray<FGameplayTag> stateChain =
+	{
+		initTags.InitState_Spawned,
+		initTags.InitState_DataAvailable,
+		initTags.InitState_DataInitialized,
+		initTags.InitState_Spawned
+	};
+
+	ContinueInitStateChain(stateChain);
 }
--- CustomLyra/Source/CustomLyra/Character/CLPawnExtensionComponent.h
+++ CustomLyra/Source/CustomLyra/Character/CLPawnExtensionComponent.h
@@ -13,4 +13,13 @@ public:
 	UCLPawnExtensionComponent(const FObjectInitializer& InObjectInitializer = FObjectInitializer::Get());
 
 	virtual void OnRegister() override;
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	static const FName NAME_ActorFeatureName;
+	virtual FName GetFeatureName() const { return NAME_ActorFeatureName; }
+
+	virtual void OnActorInitStateChanged(const FActorInitStateChangedParams& Params) override;
+	virtual bool CanChangeInitState(UGameFrameworkComponentManager* Manager, FGameplayTag CurrentState, FGameplayTag DesiredState) const override;
+	virtual void CheckDefaultInitialization() override;
 };
