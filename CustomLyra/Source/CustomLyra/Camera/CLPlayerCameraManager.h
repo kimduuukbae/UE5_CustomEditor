97626b273c16e91cf9975665af5aef7d1379d5e1
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2025-04-12 18:54:08 +0900

    Update CLPlayerCameraManager.h

--- CustomLyra/Source/CustomLyra/Camera/CLPlayerCameraManager.h
+++ CustomLyra/Source/CustomLyra/Camera/CLPlayerCameraManager.h
@@ -3,6 +3,10 @@
 #include "Camera/PlayerCameraManager.h"
 #include "CLPlayerCameraManager.generated.h"
 
+#define CL_CAMERA_DEFAULT_FOV 80.0f
+#define CL_CAMERA_DEFAULT_PITCH_MIN -89.0f
+#define CL_CAMERA_DEFAULT_PITCH_MAX 89.0f
+
 UCLASS()
 class ACLPlayerCameraManager : public APlayerCameraManager
 {
